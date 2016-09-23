1960b4f2dec86c8cac30c0c5a6c1c1b4f6e0c5a9
Ahmed Lekssays <lekssaysahmed@gmail.com>
2016-09-23 21:15:58 +0100

    add markdown to post

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,22 @@
-module ApplicationHelper
-	
+module ApplicationHelper	
+	def markdown(text)
+	  	markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
+                                       no_intra_emphasis: true,
+                                       fenced_code_blocks: true,
+                                       disable_indented_code_blocks: true,
+                                       autolink: true,
+                                       tables: true,
+                                       underline: true,
+                                       highlight: true
+                                      )
+    	return markdown.render(text).html_safe
+	end
+
+	def syntax_highlighter(html)
+	  doc = Nokogiri::HTML(html)
+	  doc.search("//pre[@lang]").each do |pre|
+	    pre.replace Albino.colorize(pre.text.rstrip, pre[:lang])
+	  end
+	  doc.to_s
+	end
 end
